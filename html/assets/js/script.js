62d63ee359dc40cd2ced81346a4192cc71ab59ed
Sergiy Yakushenko <sery@ciklum.com>
2021-03-31 13:37:45 +0300

    To variants for work with source for chapters, return JWplayer

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -4,8 +4,10 @@ var b = document.querySelector("#bar");
 
 // v.addEventListener('click',play,false);
 v.addEventListener('timeupdate',update,false);
-t.addEventListener('loaded',render,false); // Bug in FF31 MAC: wrong event name
-t.addEventListener('load',render,false);
+if (t != null) {
+    t.addEventListener('loaded',render,false); // Bug in FF31 MAC: wrong event name
+    t.addEventListener('load',render,false);
+}
 
 function play() {
     if(v.paused) { v.play(); } else { v.pause(); }
@@ -19,13 +21,16 @@ function update() {
 function render() {
     var c = v.textTracks[0].cues;
     for (var i=0; i<c.length; i++) {
-        var s = document.createElement("span");
-        s.innerHTML = c[i].text;
-        s.setAttribute('data-start',c[i].startTime);
-        // s.style.width = ((c[i].endTime-c[i].startTime)/888*3932-7)+'px';
-        s.addEventListener("click",seek);
-        b.appendChild(s);
+        addChapterMenuItem(c[i]);
     }
+}
+function addChapterMenuItem(cue) {
+    var s = document.createElement("span");
+    s.innerHTML = cue.text;
+    s.setAttribute('data-start',cue.startTime);
+    // s.style.width = ((c[i].endTime-c[i].startTime)/888*3932-7)+'px';
+    s.addEventListener("click",seek);
+    b.appendChild(s);
 }
 
 function seek(e) {
@@ -52,3 +57,72 @@ fetch(source_url)
         video.appendChild(video_source);
     })
     .catch(err => { throw err });
+
+function parse_timestamp(s) {
+    //var match = s.match(/^(?:([0-9]{2,}):)?([0-5][0-9]):([0-5][0-9][.,][0-9]{0,3})/);
+    // Relaxing the timestamp format:
+    var match = s.match(/^(?:([0-9]+):)?([0-5][0-9]):([0-5][0-9](?:[.,][0-9]{0,3})?)/);
+    if (match == null) {
+        throw 'Invalid timestamp format: ' + s;
+    }
+    var hours = parseInt(match[1] || "0", 10);
+    var minutes = parseInt(match[2], 10);
+    var seconds = parseFloat(match[3].replace(',', '.'));
+    return seconds + 60 * minutes + 60 * 60 * hours;
+}
+
+// https://w3c.github.io/webvtt/
+// https://developer.mozilla.org/en/docs/Web/API/Web_Video_Text_Tracks_Format
+// https://en.wikipedia.org/wiki/WebVTT
+//
+// For better parsers, look at:
+// https://github.com/annevk/webvtt
+// https://github.com/mozilla/vtt.js
+function quick_and_dirty_vtt_or_srt_parser(vtt) {
+    var lines = vtt.trim().replace('\r\n', '\n').split(/[\r\n]/).map(function(line) {
+        return line.trim();
+    });
+    var cues = [];
+    var start = null;
+    var end = null;
+    var payload = null;
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].indexOf('-->') >= 0) {
+            var splitted = lines[i].split(/[ \t]+-->[ \t]+/);
+            if (splitted.length != 2) {
+                throw 'Error when splitting "-->": ' + lines[i];
+            }
+
+            // Already ignoring anything past the "end" timestamp (i.e. cue settings).
+            start = parse_timestamp(splitted[0]);
+            end = parse_timestamp(splitted[1]);
+        } else if (lines[i] == '') {
+            if (start && end) {
+                var cue = new VTTCue(start, end, payload);
+                cues.push(cue);
+                start = null;
+                end = null;
+                payload = null;
+            }
+        } else if(start && end) {
+            if (payload == null) {
+                payload = lines[i];
+            } else {
+                payload += '\n' + lines[i];
+            }
+        }
+    }
+    if (start && end) {
+        var cue = new VTTCue(start, end, payload);
+        cues.push(cue);
+    }
+
+    return cues;
+}
+
+var inner_chapters = document.getElementById('innerChapters');
+if (inner_chapters != null) {
+    quick_and_dirty_vtt_or_srt_parser(inner_chapters.innerHTML).map(function(cue) {
+        addChapterMenuItem(cue);
+    });
+}
